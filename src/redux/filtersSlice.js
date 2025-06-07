d7fcccd94f145a6eace2a1e46a8d204684f00c41
Denys Almazov <almazovdenys@gmail.com>
2025-06-07 14:49:24 +0300

    added filtersSlice

---
+++ src/redux/filtersSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const filtersSlice = createSlice({
+    name: "filters",
+    initialState: {
+        location: "",
+        transmission: "",
+        equipment: [],
+        type: "",
+    },
+    reducers: {
+    changeFilter(state, action) {
+      state.name = action.payload;
+    },
+  },
+});
+
+export const { changeFilter } = filtersSlice.actions;
+
+export default filtersSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,27 @@
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  persistStore,
+} from "redux-persist";
 import { configureStore } from "@reduxjs/toolkit";
-
-import campersReducer from "./campersSlice";
+import { campersReducer } from "./campers/campersSlice";
+import { filtersReducer } from "./filtersSlice";
 
 export const store = configureStore({
   reducer: {
     campers: campersReducer,
+    filters: filtersReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
